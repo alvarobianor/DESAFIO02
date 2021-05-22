d8c67d8caee553329294507023862c11c69a7dd8
alvaro bianor <bianor.alvaro12@gmail.com>
2021-05-22 00:18:10 -0300

    Cleaning unused imports

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useState,
-  useEffect,
-} from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
 import { Product, Stock } from "../types";
